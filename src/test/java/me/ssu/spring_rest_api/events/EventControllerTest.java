f9989bbc1db7d74b074d8ea5f7c17f292cba19d2
Ssusso <zzanggoon8@gmail.com>
2021-11-11 18:24:25 +0900

    Fail on Unknown Properties

--- src/test/java/me/ssu/spring_rest_api/events/EventControllerTest.java
+++ src/test/java/me/ssu/spring_rest_api/events/EventControllerTest.java
@@ -2,6 +2,7 @@ package me.ssu.spring_rest_api.events;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -34,14 +35,17 @@ public class EventControllerTest {
 //    EventRepository eventRepository;
 
     @Test
+    @DisplayName("입력값이 제대로 들어오는 경우")
     void createEvent() throws Exception {
-        Event event = Event.builder()
-                // TODO id, free, offline 값 제한하기
-                .id(100)
-                .free(true)
-                // TODO location 값이 있을 때 true가 맞음.
+        EventDto event = EventDto.builder()
+                // TODO 입력값이 제대로 들어온 경우이기 때문에
+                // TODO id, free, offline 주석처리
+//                // TODO id, free, offline 값 제한하기
+//                .id(100)
+//                .free(true)
+//                // TODO location 값이 있을 때 true가 맞음.
+//                .offline(false)
                 .location("강남역 D2 스타일 펙토리")
-                .offline(false)
                 .name("Spring")
                 .description("REST API Development with Spring")
                 .beginEnrollmentDateTime(LocalDateTime
@@ -55,7 +59,8 @@ public class EventControllerTest {
                 .basePrice(100)
                 .maxPrice(200)
                 .limitOfEnrollment(100)
-                .build();
+                .build()
+        ;
         // TODO mock 객체 nullpoint 해결하기
         // TODO event object가 있어야 return이 가능함.
         // TODO 예제에서 mockito 해제
@@ -77,13 +82,40 @@ public class EventControllerTest {
                 // TODO 입력값 제한하기
                 // TODO id 100이면 안되며, free가 true면 안 됨.
                 .andExpect(jsonPath("id").value(Matchers.not(100)))
-                .andExpect(jsonPath("free").value(Matchers.not(true)));
+                .andExpect(jsonPath("free").value(Matchers.not(true)))
+        ;
+    }
+
+    @Test
+    @DisplayName("입력값 이외에 에러 발생")
+    void createEvent_Bad_Request() throws Exception {
+        Event event = Event.builder()
+                .id(100)
+                .free(true)
+                .location("강남역 D2 스타일 펙토리")
+                .offline(false)
+                .name("Spring")
+                .description("REST API Development with Spring")
+                .beginEnrollmentDateTime(LocalDateTime
+                        .of(2021,12, 23, 21,50))
+                .closeEnrollmentDateTime(LocalDateTime
+                        .of(2021,12, 24,22,30))
+                .beginEventDateTime(LocalDateTime
+                        .of(2021,12,25,22,55))
+                .endEventDateTime(LocalDateTime
+                        .of(2021,12,26,20,00))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .build();
+
+        mockMvc.perform(post("/api/events")
+                        // TODO Location 헤더에 생성된 이벤트 조회할 수 있는 URI 확인
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .accept(MediaTypes.HAL_JSON)
+                        .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+       ;
     }
 }
-
-
-
-
-
-
-
