3d88178331528714b0c0e67558a235b14e60e6a7
Ssusso <zzanggoon8@gmail.com>
2021-11-25 12:32:38 +0900

    HATEOAS&RESTDocs Completed

--- src/test/java/me/ssu/spring_rest_api/events/EventControllerTest.java
+++ src/test/java/me/ssu/spring_rest_api/events/EventControllerTest.java
@@ -217,8 +217,8 @@ public class EventControllerTest {
                         .content(objectMapper.writeValueAsString(eventDto)))
                 .andDo(print())
                 .andExpect(status().isBadRequest())
-                // TODO Unwrap 수정
-                // TODO $[0] -> content[0]
+                // TODO Unwrap 수정(Json에는 Unwrap가 적용 안 됨)
+                // TODO $[0](Error) -> content[0](ErrorsResource)
                 .andExpect(jsonPath("content[0].objectName").exists())
                 .andExpect(jsonPath("content[0].field").exists()) // 어떤 필드에서 발생한 에러인지
                 .andExpect(jsonPath("content[0].defaultMessage").exists()) // 기본 메시지
@@ -228,42 +228,6 @@ public class EventControllerTest {
 //                .andExpect(jsonPath("$[0].rejectValue").exists()) // 입력 거절 받은 값
                 // TODO 인덱스로 가는 링크 제공
                 .andExpect(jsonPath("_links.index").exists())
-        ;
-    }
-
-    @Autowired
-    EventRepository eventRepository;
-
-    private void generateEvent(int index) {
-        Event event = Event.builder()
-                .name("event" + index)
-                .description("test event")
-                .build();
-
-        eventRepository.save(event);
-    }
-
-    @Test
-    @DisplayName("30개의 이벤트를 10개씩 두 번째 페이지 조회하기")
-    void queryEvents() throws Exception {
-        // TODO Given
-        IntStream.range(0, 30).forEach(this::generateEvent);
-//        IntStream.range(0, 30).forEach(i -> {
-//            generateEvent(i);
-//        });
-
-        // TODO When
-        mockMvc.perform(get("/api/events")
-                        .param("page", "1")
-                        .param("size", "10")
-                        .param("sort", "name,DESC"))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("page").exists())
-                .andExpect(jsonPath("_embedded.eventList[0].self").exists())
-//                .andExpect(jsonPath("_links.self").exists())
-//                .andExpect(jsonPath("_links.profile").exists())
-//                .andDo(document("query-events"))
         ;
     }
 }
