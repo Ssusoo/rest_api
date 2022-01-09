72d7d8b1e1194854b23758a12df7cc3992f045e7
zzanggoon8 <zzanggoon8@gmail.com>
2022-01-10 00:09:57 +0900

    Add Account Adapter

--- src/main/java/me/ssu/springrestapi/accounts/AccountAdapter.java
+++ src/main/java/me/ssu/springrestapi/accounts/AccountAdapter.java
@@ -18,6 +18,7 @@ public class AccountAdapter extends User {
     }
 
     public AccountAdapter(Account account) {
+        // TODO 부모 new User(account.getEmail(), account.getPassword(), authorities(account.getRoles()))
         super(account.getEmail(), account.getPassword(), authorities(account.getRoles()));
         this.account = account;
     }
